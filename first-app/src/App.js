d40747c9ee7f2f77d91123fd2f402b1568ca4098
hardzal <suryadijogja@gmail.com>
2022-06-07 14:18:17 +0700

    menambahkan data

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const profiles = {
+    name: 'M Rizal',
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, {profiles.name}</h1>
     </div>
   );
 }
